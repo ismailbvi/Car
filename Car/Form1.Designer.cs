d273e5bb40b39625bbc8d4b2355f91b881ccde7d
ismailbvi <ismailbi@abv.bg>
2025-01-21 19:49:37 +0200

    "1 commit"

---
+++ Car/Form1.Designer.cs
@@ -0,0 +1,211 @@
+﻿namespace idk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.labelPrice = new System.Windows.Forms.Label();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.labelDiscount = new System.Windows.Forms.Label();
+            this.labelTotal = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Lucida Sans Unicode", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(43, 39);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(535, 52);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.Text = "Car Model";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(222, 128);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 36);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Цена";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(23, 128);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(202, 36);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Базова цена:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(246, 269);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1065, 1127);
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox1.Location = new System.Drawing.Point(29, 284);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(71, 24);
+            this.checkBox1.TabIndex = 4;
+            this.checkBox1.Text = "ABS";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox2.Location = new System.Drawing.Point(29, 314);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(183, 24);
+            this.checkBox2.TabIndex = 5;
+            this.checkBox2.Text = "Фарове за мъгла";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.checkBox3.Location = new System.Drawing.Point(29, 344);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(136, 24);
+            this.checkBox3.TabIndex = 6;
+            this.checkBox3.Text = "Парктроник";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.AutoSize = true;
+            this.buttonCalculate.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonCalculate.Location = new System.Drawing.Point(58, 848);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(163, 64);
+            this.buttonCalculate.TabIndex = 7;
+            this.buttonCalculate.Text = "Пресметни";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            // 
+            // labelPrice
+            // 
+            this.labelPrice.AutoSize = true;
+            this.labelPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPrice.Location = new System.Drawing.Point(550, 1077);
+            this.labelPrice.Name = "labelPrice";
+            this.labelPrice.Size = new System.Drawing.Size(0, 40);
+            this.labelPrice.TabIndex = 9;
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.AutoSize = true;
+            this.buttonReset.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonReset.Location = new System.Drawing.Point(58, 918);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(163, 59);
+            this.buttonReset.TabIndex = 10;
+            this.buttonReset.Text = "Изход";
+            this.buttonReset.UseVisualStyleBackColor = true;
+            // 
+            // labelDiscount
+            // 
+            this.labelDiscount.AutoSize = true;
+            this.labelDiscount.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelDiscount.Location = new System.Drawing.Point(550, 1128);
+            this.labelDiscount.Name = "labelDiscount";
+            this.labelDiscount.Size = new System.Drawing.Size(0, 40);
+            this.labelDiscount.TabIndex = 11;
+            // 
+            // labelTotal
+            // 
+            this.labelTotal.AutoSize = true;
+            this.labelTotal.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelTotal.Location = new System.Drawing.Point(550, 1182);
+            this.labelTotal.Name = "labelTotal";
+            this.labelTotal.Size = new System.Drawing.Size(0, 40);
+            this.labelTotal.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1313, 1455);
+            this.Controls.Add(this.labelTotal);
+            this.Controls.Add(this.labelDiscount);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.labelPrice);
+            this.Controls.Add(this.buttonCalculate);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label labelPrice;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Label labelDiscount;
+        private System.Windows.Forms.Label labelTotal;
+    }
+}
+
---
+++ Car/Form1.cs
@@ -0,0 +1,209 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace idk
+{
+    public partial class Form1 : Form
+    {
+        private Dictionary<string, int> extraPrices = new Dictionary<string, int>
+        {
+            { "Skoda Fabia", 19640 },
+            { "Ford Focus", 25990 },
+            { "Toyota Land Cruiser", 21850 },
+            { "Ferrari", 30990 }
+        };
+        private Dictionary<string, int> checkboxPrices = new Dictionary<string, int>
+{
+            { "Feature 1", 500 },
+            { "Feature 2", 1000 },
+            { "Feature 3", 1500 }
+};
+        public Form1()
+        {
+            InitializeComponent();
+            InitializeCarModels();
+            comboBox1.SelectedIndexChanged += ComboBox1_SelectedIndexChanged;
+            buttonReset.Click += buttonReset_Click;
+        }
+        private void InitializeCarModels()
+        {
+            comboBox1.Items.Clear();
+
+            comboBox1.Items.AddRange(extraPrices.Keys.ToArray());
+            buttonCalculate.Click += buttonCalculate_Click;
+
+
+
+        }
+        private Dictionary<string, string> carImages = new Dictionary<string, string>
+{
+          { "Skoda Fabia", "img/skoda-fabia-monte-carlo-tech-07-04-13.jpg" },
+          { "Ford Focus", "img/s-l400.jpg" },
+    { "Toyota Land Cruiser", "img/151902413.jpg" },
+    { "Ferrari", "images/1c5438761d631e2f699a82f72ce5aef3.jpg" }
+};
+        private void ComboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string basePath = Path.Combine(Application.StartupPath, "img");
+            Console.WriteLine($"Base path: {basePath}"); // Debug output
+
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedModel = comboBox1.SelectedItem.ToString();
+
+                string imagePath = string.Empty;
+
+                switch (selectedModel)
+                {
+                    case "Skoda Fabia":
+                        label1.Text = $"{extraPrices["Skoda Fabia"]:N0}лв";
+                        imagePath = Path.Combine(basePath, "skoda-fabia-monte-carlo-tech-07-04-13.jpg");
+                        break;
+                    case "Ford Focus":
+                        label1.Text = $"{extraPrices["Ford Focus"]:N0}лв";
+                        imagePath = Path.Combine(basePath, "s-l400.jpg");
+                        break;
+                    case "Toyota Land Cruiser":
+                        label1.Text = $"{extraPrices["Toyota Land Cruiser"]:N0}лв";
+                        imagePath = Path.Combine(basePath, "151902413.jpg");
+                        break;
+                    case "Ferrari":
+                        label1.Text = $"{extraPrices["Ferrari"]:N0}лв";
+                        imagePath = Path.Combine(basePath, "1c5438761d631e2f699a82f72ce5aef3.jpg");
+                        break;
+                    default:
+                        label1.Text = string.Empty;
+                        pictureBox1.Image = null;
+                        return;
+                }
+
+                Console.WriteLine($"Image path: {imagePath}"); 
+                if (File.Exists(imagePath))
+                {
+                    pictureBox1.Image = Image.FromFile(imagePath);
+                }
+            
+
+                comboBox1.Enabled = false;
+                comboBox1.Text = selectedModel;
+            }
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            int basePrice = 0;
+
+            
+            if (comboBox1.SelectedItem != null)
+            {
+                string selectedModel = comboBox1.SelectedItem.ToString();
+                if (extraPrices.TryGetValue(selectedModel, out int price))
+                {
+                    basePrice = price;
+                }
+            }
+
+            
+            int extraPrice = 0;
+            if (checkBox1.Checked)
+            {
+                extraPrice += checkboxPrices["Feature 1"];
+            }
+            if (checkBox2.Checked)
+            {
+                extraPrice += checkboxPrices["Feature 2"];
+            }
+            if (checkBox3.Checked)
+            {
+                extraPrice += checkboxPrices["Feature 3"];
+            }
+
+            
+            int totalPriceBeforeDiscount = basePrice + extraPrice;
+
+           
+            double discountAmount = 0;
+            double totalPriceAfterDiscount = totalPriceBeforeDiscount;
+
+            
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+            {
+                discountAmount = totalPriceBeforeDiscount * 0.01; 
+                totalPriceAfterDiscount = totalPriceBeforeDiscount - discountAmount;
+            }
+
+            
+            labelPrice.Text = $"Цена с избрани екстри: {totalPriceBeforeDiscount:N0}лв";
+
+            
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+            {
+                labelDiscount.Text = $"Отстъпка(1%): {discountAmount:N0}лв";
+            }
+            else
+            {
+                labelDiscount.Text = string.Empty; 
+            }
+
+            
+            if (checkBox1.Checked && checkBox2.Checked && checkBox3.Checked)
+            {
+                labelTotal.Text = $"Общо: {totalPriceAfterDiscount:N0}лв";
+            }
+            else
+            {
+                labelTotal.Text = $"Общо: {totalPriceBeforeDiscount:N0}лв"; 
+            }
+        }
+        private void InitializeCheckBoxes()
+        {
+            checkBox1.Text = $"Feature 1 (+${checkboxPrices["Feature 1"]:N0})";
+            checkBox2.Text = $"Feature 2 (+${checkboxPrices["Feature 2"]:N0})";
+            checkBox3.Text = $"Feature 3 (+${checkboxPrices["Feature 3"]:N0})";
+        }
+
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+
+            comboBox1.SelectedIndex = -1;
+            comboBox1.Text = string.Empty;
+
+
+            checkBox1.Checked = false;
+            checkBox2.Checked = false;
+            checkBox3.Checked = false;
+
+
+            label1.Text = string.Empty;
+            labelPrice.Text = string.Empty;
+            labelDiscount.Text = string.Empty; 
+            labelTotal.Text = string.Empty;
+
+            pictureBox1.Image = null;
+
+
+            comboBox1.Enabled = true;
+
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
